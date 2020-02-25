17e99f36a1a530ca3cceb18113f438ee99853cf5
Izzi <izzikoning@me.com>
2020-02-25 15:38:28 +0200

    Changes to config of site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Document by Code Bushi`,
-    name: `Code Bushi`,
-    siteUrl: `https://gatsby-theme-document.netlify.com`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    title: `A System of Worlds`,
+    name: `Izzi Ink`,
+    siteUrl: `https://hopeful-badger.netlify.com`,
+    description: `This is a collection of learning material to master JavaScript, CSS and HTML`,
     social: [
       {
         name: `github`,
-        url: `https://github.com/codebushi/gatsby-theme-document`
+        url: `https://github.com/Izmargad/world-docs`
       },
       {
         name: `twitter`,
-        url: `https://twitter.com/HuntaroSan`
+        url: `https://twitter.com/golem37`
       }
     ],
     sidebarConfig: {
